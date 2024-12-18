62aedb0483c78f73248f4403e9e02460e8764b13
nidhinsankar <nidhinsankar1@gmail.com>
2024-12-18 15:16:28 +0530

    added banner-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-function App() {
-  return (
-    <>
-      <div className="bg-red-400 font-bold text-center">
-        <p>
-          A react template repository with tailwindcss, Typescript scaffolded by
-          vite
-        </p>
-        <h2>Feel free to start building your project</h2>
-      </div>
-    </>
-  );
-}
+import "./index.css";
+import BannerCardItem from "./components/BannerItem";
+
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: "The Seasons Latest",
+    description: "Get the seasons all latest designs in a flick of your hand",
+    className: "card-1",
+  },
+  {
+    id: 2,
+    headerText: "Our New Designs",
+    description:
+      "Get the designs developed by our in-house team all for yourself",
+    className: "card-2",
+  },
+  {
+    id: 3,
+    headerText: "Insiders",
+    description: "Get the top class products for yourself with an extra off",
+    className: "card-3",
+  },
+];
+
+const App = () => (
+  <div className="bg-container">
+    <ul className="banner-card-list">
+      {bannerCardsList.map((eachcard) => (
+        <BannerCardItem bannerCard={eachcard} key={eachcard.id} />
+      ))}
+    </ul>
+  </div>
+);
 
 export default App;
